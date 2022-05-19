5ff4ceea22a6572916d69d525deb3e06439a0cdd
juliangander <gander.julian@hotmail.com>
2022-05-19 15:27:16 +0200

    button

--- sketch.js
+++ sketch.js
@@ -1,1 +1,46 @@
-asdf as SVGDefsElement
+let button;
+let i=0;
+let direction = true;
+let text = [
+    "Hallo!",
+    "Wie geht es dir?"];
+let displayedText = text[0];
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  button = createButton('click me');
+  button.position(windowWidth/2, windowHeight/2);
+  button.style('background-color', color(0,0,255))
+  button.size(windowWidth/3, windowHeight/3)
+  button.center()
+   button.mousePressed(startGame);
+
+text()
+
+}
+
+ 
+
+function draw() {
+
+    background(0)
+    fill(255,0,0);
+    rect(i,30,20);
+
+    if(i>=windowWidth){
+        direction = false;
+    } else if(i<=0){
+        direction = true;
+    }  
+
+
+    if(direction == true){
+        i+=5;
+    } else {
+        i-=5;
+    }  
+    }
+
+    function startGame() {
+        console.log("Game starts now!")
+         }
