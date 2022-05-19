62666c8c4f5f39483bb73ed1d7d9359b7432058a
juliangander <gander.julian@hotmail.com>
2022-05-19 17:11:20 +0200

    intro

--- sketch.js
+++ sketch.js
@@ -1,21 +1,23 @@
 let button;
 let i=0;
+let j=1
 let direction = true;
-let text = [
-    "Hallo!",
-    "Wie geht es dir?"];
-let displayedText = text[0];
+let intro = [
+    "Willkommen zum Eignungstest für Gottheiten!\n\nTestet euer göttliches Geschick. Versucht zu zweit ein Monument für die Menschen im Dorf zu bauen.\n\nEine Person baut, während die andere schaut und Anweisungen gibt.",
+    "Bevor ihr jedoch mit dem Bau beginnen könnt, müsst ihr das von einem Erdbeben verwüstete Dorf aufräumen.\n\nStellt dazu die umgefallenen Gebäude abwechslungsweise wieder auf. Die Lichter zeigen euch ob diese korrekt stehen.\n\nSprecht euch ab wer mit bauen beginnt.",
+    "Ihr habt nun einen Tag Zeit euer Monument auf dem Baufeld in der Mitte des Dorfes zu bauen.\n\nPro Spielzug/Spieleinsatz dürft ihr vier Elemente berühren/verschieben. Danach wird gewechselt.\n\nGestapelte Elemente dürfen miteinander verschoben werden, sofern nur ein Element berührt wird.\n\nFangt nun an zu bauen!"];
+let displayedText = intro[0];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  button = createButton('click me');
+  button = createButton('Weiter');
   button.position(windowWidth/2, windowHeight/2);
-  button.style('background-color', color(0,0,255))
-  button.size(windowWidth/3, windowHeight/3)
+  button.style('background-color', color(0))
+  button.style('color', 'white');
+  button.style('font-size', 'windowWidth/20');
+  button.size(windowWidth/5, windowHeight/8)
   button.center()
    button.mousePressed(startGame);
-
-text()
 
 }
 
@@ -39,8 +41,16 @@ function draw() {
     } else {
         i-=5;
     }  
+    textAlign(CENTER);
+    textSize(windowWidth/80)
+    fill(255)
+    text(displayedText, windowWidth/2, 100);
+
     }
 
     function startGame() {
         console.log("Game starts now!")
+        displayedText = intro[j]
+        j=j+1
          }
+        
